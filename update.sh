9fc3f59880cd35e53df12c61df2a8be8beae2a68
Josh Hill <josh.hill@makers.tech>
2019-08-09 00:35:43 +0100

    Skip setting user shell if already set
    Avoid prompt for password

--- update.sh
+++ update.sh
@@ -90,12 +90,18 @@ remove_icons_from_dock() {
 
 set_shell() {
   log "** Set shell"
-  if ! grep "/usr/local/bin/bash" /etc/shells > /dev/null 2>&1; then
-    echo '/usr/local/bin/bash' | sudo tee -a /etc/shells > /dev/null
+  local homebrew_bash="/usr/local/bin/bash"
+  if ! grep $homebrew_bash /etc/shells > /dev/null 2>&1; then
+    echo $homebrew_bash | sudo tee -a /etc/shells > /dev/null
     echo "Added Homebrew bash to system shells."
   fi
 
-  sudo chsh -s /usr/local/bin/bash
+  local user_shell
+  user_shell=$(dscl /Local/Default -read "/Users/$USER" UserShell | cut -d' ' -f2)
+  if [ "$user_shell" = $homebrew_bash ]; then
+    return
+  fi
+
   chsh -s /usr/local/bin/bash
 }
 
