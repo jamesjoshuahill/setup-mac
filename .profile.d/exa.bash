686ed5f779d5cc8518239e8b253d7f5fba782c5e
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-04-20 17:58:23 +0100

    Split aliases into application-specific scripts

---
+++ .profile.d/exa.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+alias ll='exa --long --all --git'
+alias tree='exa --tree'
---
+++ .profile.d/setup-git.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# https://www.atlassian.com/git/tutorials/dotfiles
+alias setup-git='git --git-dir="$HOME/.setup/" --work-tree="$HOME"'
+alias s='setup-git'
