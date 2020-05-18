01920ab213f5d502d735c1107ae070b8e6876124
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-05-18 09:13:02 +0100

    Color prompt red when last command exits non-zero

--- .config/profile.d/ps1.bash
+++ .config/profile.d/ps1.bash
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-export PS1='\[\033[01;34m\]\w\[\033[0m\] '
+export PROMPT_COMMAND="__prompt_command;$PROMPT_COMMAND"
+
+__prompt_command() {
+  # shellcheck disable=SC2181
+  if [[ $? -eq 0 ]]
+  then
+    export PS1='\[\033[01;34m\]\w\[\033[0m\] '
+  else
+    export PS1='\[\033[01;31m\]\w\[\033[0m\] '
+  fi
+}
