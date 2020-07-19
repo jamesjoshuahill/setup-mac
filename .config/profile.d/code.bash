aff66fb9f6bd92ecbf4918fc3076930fd7d81964
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2020-07-19 22:56:35 +0100

    Only install missing vs code extensions

--- .config/profile.d/code.bash
+++ .config/profile.d/code.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-alias install_vscode_extensions='xargs -n 1 code --force --install-extension < "$HOME/Library/Application Support/Code/User/extensions.txt"'
-alias update_vscode_extension_list='code --list-extensions > "$HOME/Library/Application Support/Code/User/extensions.txt"'
+alias update_vscode_extension_list='code --list-extensions > "$HOME/Library/ApplicationSupport/Code/User/extensions.txt"'
+
+function install_vscode_extensions() {
+  comm -13 <(code --list-extensions) "$HOME/Library/ApplicationSupport/Code/User/extensions.txt" |
+    xargs -n 1 code --force --install-extension
+}
