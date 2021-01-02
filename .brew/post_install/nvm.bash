4813fd685878aef7bc9b12eff82ee15a0bcba1ab
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2021-01-02 18:51:02 +0000

    Workaround unbound variable in nvm

--- .brew/post_install/nvm.bash
+++ .brew/post_install/nvm.bash
@@ -6,8 +6,10 @@ install_node() {
   log "Install Node.js ${default_node_version}"
 
   export NVM_DIR="$HOME/.nvm"
+  set +u
   # shellcheck disable=SC1091
   source "/usr/local/opt/nvm/nvm.sh"
+  set -u
 
   nvm install $default_node_version
   nvm alias default $default_node_version
